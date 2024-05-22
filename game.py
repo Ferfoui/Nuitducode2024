67ce8d6aa8056df3e13de56b4a854233233a3bad
Ferfoui <kenji.oku@outlook.fr>
2024-05-22 13:34:10 +0200

    Création de la classe Game

---
+++ game.py
@@ -0,0 +1,17 @@
+import pyxel
+
+SCREEN_SIZE = 128
+GAME_NAME = "Nuit du code 2024"
+
+class Game:
+    def __init__(self):
+        pyxel.init(SCREEN_SIZE, SCREEN_SIZE, GAME_NAME)
+    
+    def update(self):
+        pass
+    
+    def draw(self):
+        pass
+        
+
+Game()
