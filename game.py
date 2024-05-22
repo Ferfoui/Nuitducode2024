d0f92e056a22ed35586f3aa82b5246e5bede401c
Ferfoui <kenji.oku@outlook.fr>
2024-05-22 14:37:01 +0200

    Ajout de la texture

--- game.py
+++ game.py
@@ -13,33 +13,36 @@ class Player:
         self.y = y  
 
     def update(self):
-        if pyxel.btn(pyxel.KEY_LEFT) and self.x>0:
+        if pyxel.btn(pyxel.KEY_LEFT) and (self.x > 0):
             self.move(-1,2)
-        if pyxel.btn(pyxel.KEY_RIGHT) and self.x<SCREEN_SIZE:
+        if pyxel.btn(pyxel.KEY_RIGHT) and (self.x < SCREEN_SIZE):
             self.move(1,2)
 
-
     def draw(self):
-        pyxel.blt(self.x, self.y, 0, 0, 8, 8, 8 ,colkey=12)
+        pyxel.blt(self.x, self.y, 0, 0, 8, 16, 16, colkey=12)
 
     def move(self, direction, speed):
-        self.x +=direction*speed
+        self.x += direction * speed
 
 
 ### GAME CLASSE ###
 
 class Game:
     def __init__(self):
-        pyxel.init(SCREEN_SIZE, SCREEN_SIZE, GAME_NAME, quit_key = pyxel.KEY_ESCAPE)
-        #pyxel.image[0].load(0,0,"1.pyxres")  ne marche pas
+        self.pyxel_init()
+        
         self.player = Player(0,0)
         pyxel.run(self.update, self.draw)
+    
+    def pyxel_init(self):
+        pyxel.init(SCREEN_SIZE, SCREEN_SIZE, GAME_NAME, quit_key = pyxel.KEY_ESCAPE)
+        pyxel.load('1.pyxres')
     
     def update(self):
         self.player.update()
     
     def draw(self):
-        pyxel.cls(12)
+        pyxel.cls(5)
         self.player.draw()
         
         
