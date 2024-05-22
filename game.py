4856303cf0daec3c8afc69a39e9a0bc486f4cef4
Euddhkq <metelkinealexis@gmail.com>
2024-05-22 13:50:44 +0200

    reglage d'un bug

--- game.py
+++ game.py
@@ -6,7 +6,7 @@ GAME_NAME = "Nuit du code 2024"
 class Game:
     def __init__(self):
         pyxel.init(SCREEN_SIZE, SCREEN_SIZE, GAME_NAME, quit_key=pyxel.KEY_ESCAPE)
-        pyxel.run(self.update(),self.draw())
+        pyxel.run(self.update,self.draw)
     
     def update(self):
         pass
