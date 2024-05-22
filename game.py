4f73427be02a7c6d7ab3eec3491618cffd03aa34
Euddhkq <metelkinealexis@gmail.com>
2024-05-22 13:47:45 +0200

    ajout d'une boucle

--- game.py
+++ game.py
@@ -5,7 +5,8 @@ GAME_NAME = "Nuit du code 2024"
 
 class Game:
     def __init__(self):
-        pyxel.init(SCREEN_SIZE, SCREEN_SIZE, GAME_NAME)
+        pyxel.init(SCREEN_SIZE, SCREEN_SIZE, GAME_NAME, quit_key=pyxel.KEY_ESCAPE)
+        pyxel.run(self.update(),self.draw())
     
     def update(self):
         pass
