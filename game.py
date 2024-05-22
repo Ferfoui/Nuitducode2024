d344916504593f9b5b6fa02234e8e0f6d8541e90
Ferfoui <kenji.oku@outlook.fr>
2024-05-22 13:55:32 +0200

    Ajout d'une petite structure de code

--- game.py
+++ game.py
@@ -1,18 +1,26 @@
 import pyxel
 
+### CONSTANTS ###
+
 SCREEN_SIZE = 128
 GAME_NAME = "Nuit du code 2024"
+
+### IN GAME CLASSES ###
+
+
+
+### GAME CLASSE ###
 
 class Game:
     def __init__(self):
         pyxel.init(SCREEN_SIZE, SCREEN_SIZE, GAME_NAME, quit_key=pyxel.KEY_ESCAPE)
-        pyxel.run(self.update,self.draw)
+        pyxel.run(self.update, self.draw)
     
     def update(self):
         pass
     
     def draw(self):
-        pass
+        pyxel.cls(0)
         
 
 Game()
