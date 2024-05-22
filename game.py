c05b64c1fd0324d0b1329a6e1f68a498dfbaf6d8
Euddhkq <metelkinealexis@gmail.com>
2024-05-22 15:34:36 +0200

    borders qui marchent pas

--- game.py
+++ game.py
@@ -26,30 +26,43 @@ class Player:
     def move(self, direction, speed):
         self.x += direction * speed
 
+## Border
 class Border:
-    def __init__(self, width):
+    def __init__(self, x, y, width, color:int):
         self.width = width
+        self.y = y
+        self.x = x
+        self.color = color
 
-    def update(self):
-        pass
+    def move_down(self, scroll_speed):
+        self.y -= scroll_speed
 
     def draw(self):
-        pyxel.blt()
+        pyxel.blt(self.x, self.y, 0, self.color*8, 8, 8, 0)
+
+
 
 ## World
 class World:
     def __init__(self):
         self.obstacle_list = []
+        self.border_list = [Border(0,i*8,8,8+i%2) for i in range((SCREEN_SIZE//8)-1)]
             
     def scroll_world(self, scroll_value):
         for obstacle in self.obstacle_list:
             obstacle.go_down(scroll_value)
+
+ #       for border in self.border_list:
+  #          border.move_down(scroll_value)
+   #         if border.y>SCREEN_SIZE:# supprime les border si elles sortent
+    #            self.border_list.remove(border)
     
     def update():
         pass
     
-    def draw():
-        pass
+    def draw(self):
+        for border in self.border_list:
+            border.draw()
 
 class Obstacle:
     def __init__(self, x, y):
@@ -69,7 +82,7 @@ class Obstacle:
 class Game:
     def __init__(self):
         self.pyxel_init()
-        
+        self.world = World()
         self.player = Player(0,0)
         pyxel.run(self.update, self.draw)
     
@@ -78,10 +91,13 @@ class Game:
         pyxel.load('1.pyxres')
     
     def update(self):
+        self.world.scroll_world(2)
         self.player.update()
+
     
     def draw(self):
         pyxel.cls(3)
+        self.world.draw()
         self.player.draw()
         
         
